b94977ec1a6b87ffb819a61ddea1221bc1b71f39
ajeeshms <ajeesh@paskan.com>
2014-05-20 15:04:38 +0530

    Primary files and folders

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            js: {
+                src: [
+                    'src/intro.js',
+                    'src/lib/sizzle.js',
+                    'src/outro.js'
+                ],
+                dest: 'build/storm.js'
+            }
+        },
+        uglify: {
+            js: {
+                files: {
+                    'combined.js': ['combined.js']
+                }
+            }
+        },
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['concat:css', 'cssmin:css', 'concat:js', 'uglify:js']);
+};
---
+++ src/intro.js
@@ -0,0 +1,17 @@
+﻿/*
+ * Storm JavaScript MVC Framework v0.1.0
+ * Copyright 2014 Ajeesh M Sudhakaran
+ * Date: 20/05/2014
+*/
+(function (window) {
+
+    'use strict';
+    var storm = function () {
+        return new storm();
+    }
+
+    storm.about = {
+        version: '0.1.0',
+        author: 'Ajeesh M Sudhakaran',
+        date: '20/05/2014'
+    }
